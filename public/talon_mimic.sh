ca812b6fcbe423dce8879bd4be54b19269ecaf41
Trillium Smith <spiteless@gmail.com>
2024-05-24 11:04:31 -0700

    feat: Refactor mimic funcs to wake talon if sleeping

--- public/talon_mimic.sh
+++ public/talon_mimic.sh
@@ -4,13 +4,11 @@ source ~/bashrc_dir/private/path.sh
 
 function m() {
   # Check if Talon is asleep
-  talon_state=$(state)
-  if [[ "$talon_state" == "Talon is asleep" ]]; then
-    # Wake up Talon
-    echo "mimic(\"talon wake\")"| $TALON_REPL_PATH > /dev/null
-    # Pipe a short sleep between each command
-    echo "actions.sleep(.05)"| $TALON_REPL_PATH > /dev/null
-  fi
+  state
+  # Capture output of last command with $? (either 0 or 1)
+  # Assign talon_state based on $?
+  talon_state=$(if [ $? -eq 0 ]; then echo "True"; else echo "False"; fi)
+  state $talon_state start
 
   # begin an empty command
   command=""
@@ -41,23 +39,16 @@ function m() {
   echo "mimic(\"${command/ /}\")"| $TALON_REPL_PATH > /dev/null
 
   # If Talon was initially asleep, put it back to sleep
-  if [[ "$talon_state" == "Talon is asleep" ]]; then
-    # Pipe a short sleep between each command
-    echo "actions.sleep(.05)"| $TALON_REPL_PATH > /dev/null
-    # Put Talon to sleep
-    echo "mimic(\"talon sleep\")"| $TALON_REPL_PATH > /dev/null
-  fi
+  state $talon_state end
 }
 
 function M() {
   # Check if Talon is asleep
-  talon_state=$(state)
-  if [[ "$talon_state" == "Talon is asleep" ]]; then
-    # Wake up Talon
-    echo "mimic(\"talon wake\")"| $TALON_REPL_PATH > /dev/null
-    # Pipe a short sleep between each command
-    echo "actions.sleep(.05)"| $TALON_REPL_PATH > /dev/null
-  fi
+  state
+  # Capture output of last command with $? (either 0 or 1)
+  # Assign talon_state based on $?
+  talon_state=$(if [ $? -eq 0 ]; then echo "True"; else echo "False"; fi)
+  state $talon_state start
 
   # Change to the last used window
   echo "mimic(\"command tab\")"| $TALON_REPL_PATH > /dev/null
@@ -71,24 +62,29 @@ function M() {
   # (as long as no other window changing commands are used)
   echo "mimic(\"command tab\")"| $TALON_REPL_PATH > /dev/null
 
-  # If Talon was initially asleep, put it back to sleep
-  if [[ "$talon_state" == "Talon is asleep" ]]; then
-    # Pipe a short sleep between each command
-    echo "actions.sleep(.05)"| $TALON_REPL_PATH > /dev/null
-    # Put Talon to sleep
-    echo "mimic(\"talon sleep\")"| $TALON_REPL_PATH > /dev/null
-  fi
+  state $talon_state end
 }
 
 function state() {
   # Capture the output of the actions.speech.enabled() command
   talon_state=$(echo "actions.speech.enabled()" | $TALON_REPL_PATH | tail -n 1)
 
+  # If the first argument is False, execute the echo command
+  if [[ $1 == "False" ]]; then
+    if [[ $2 == "start" ]]; then
+    echo "mimic(\"talon wake\")"| $TALON_REPL_PATH > /dev/null
+    fi
+
+    if [[ $2 == "end" ]]; then
+    echo "mimic(\"talon sleep\")"| $TALON_REPL_PATH > /dev/null
+    fi
+  fi
+
   # Check if Talon is awake
   if [[ "$talon_state" == "True" ]]; then
-    echo "Talon is awake"
+    return 0 
   else
-    echo "Talon is asleep"
+    return 1
   fi
 }
 

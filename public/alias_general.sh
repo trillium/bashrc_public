6775f4f43b9fdc086444b692ba52c558752c761d
Trillium Smith <spiteless@gmail.com>
2024-05-24 20:48:14 -0700

    feat: Create alias_general, move alias' from .zshrc

---
+++ public/alias_general.sh
@@ -0,0 +1,12 @@
+alias py="python"
+alias reload="source ~/bashrc_dir/.zshrc"
+alias pbclip="pbcopy"
+alias copy="pbcopy"
+alias clip="pbcopy"
+alias clip.get="pbcopy"
+alias clip.set="pbpaste"
+alias paste="pbpaste"
+
+alias yarm='yarn'
+alias ydev='yarn dev'
+alias ya='yarn add'
